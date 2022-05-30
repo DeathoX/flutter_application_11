d9903fab2728460e447656d74d918a9cbf2e11f6
Benny Karnadi <bennykarnadi48@gmail.comm>
2022-05-30 23:00:01 +0700

    UAS_Mobileprog

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_11/login.dart';
+import 'package:flutter_application_11/menu.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
